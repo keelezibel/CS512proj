2537b9f22903eb4b6273baa304b4762f0e748a75
Le Tang <letang.boon@gmail.com>
2016-04-30 21:47:23 -0500

    change train and test

--- main.m
+++ main.m
@@ -24,17 +24,17 @@ load('data.mat');
 
 %% Pick subset of data
 % Pick top 10000 authors to train
-k = 10000;
+k = 3000;
 train_author_tab = returntopk(k, 3, train_cell, 'Author');
 train_repo_tab = returntopk(k, 1, train_cell, 'Repository');
 
 [index, ~] = cellfun(@(x) ismember(x,train_author_tab.Author), train_cell(:,3), 'UniformOutput', 0);
 train_cell_top = train_cell(cell2mat(index),:);
 train_author_top = cell2table(train_cell_top,'VariableNames',{'repo_name' 'repo_owner' 'actor' 'language'});
-writetable(train_author_top,'train_top_author.txt','Delimiter','\t');
+
 %%
 % Pick top 10 authors to test
-k = 10;
+k = 3000;
 test_author_tab = returntopk(k, 3, truth_cell, 'Author');
 [index, ~] = cellfun(@(x) ismember(x,test_author_tab.Author), truth_cell(:,3), 'UniformOutput', 0);
 test_cell_top = truth_cell(cell2mat(index),:);
@@ -46,9 +46,15 @@ flag_exist = zeros(size(test_author_top,1),1);
 flag_exist = cellfun(@(x) ismember(x,train_author_top.actor), test_author_top.actor,...
                  'UniformOutput', 0);
 test_author_top = test_author_top(cell2mat(flag_exist),:);
-writetable(test_author_top,'test_top_author.txt','Delimiter','\t');
+writetable(test_author_top,'test_top_author3000.txt','Delimiter','\t');
 
-
+%%
+[committers,ind] = unique(table2cell(test_author_top(:,3)));
+[index, ~] = cellfun(@(x) ismember(x,committers), train_cell_top(:,3), 'UniformOutput', 0);
+train_cell_top = train_cell_top(cell2mat(index),:);
+train_author_top = cell2table(train_cell_top,'VariableNames',{'repo_name' 'repo_owner' 'actor' 'language'});
+[committers_train,ind] = unique(table2cell(train_author_top(:,3)));
+writetable(train_author_top,'train_top_3000author.txt','Delimiter','\t');
 
 
 
